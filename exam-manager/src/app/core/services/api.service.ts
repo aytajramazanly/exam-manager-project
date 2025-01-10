c9765c1975a42b57919b892a8f3e4187ae3cc3e1
Aytac Ramazanli <aytajrmznl@gmail.com>
2025-01-10 14:23:49 +0400

    added core module and base api service

---
+++ exam-manager/src/app/core/services/api.service.ts
@@ -0,0 +1,61 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  protected apiUrl = environment.apiUrl;
+  private url = "";
+
+  constructor(protected http: HttpClient, @Inject(String) protected controller: string) {
+    this.url = `${this.apiUrl}/${controller}`;
+  }
+
+  getAll<T>(action: string = ""): Observable<T> {
+    return this.http.get<T>(`${this.url}${this.getActionValue(action)}`);
+  }
+
+  getById<T>(id: string, action: string = ""): Observable<T> {
+    return this.http.get<T>(`${this.url}${this.getActionValue(action)}/${id}`);
+  }
+
+  get<T>(action: string = ""): Observable<T> {
+    return this.http.get<T>(`${this.url}${this.getActionValue(action)}`);
+  }
+
+  post<T>(item: Object, action: string = ""): Observable<T> {
+    return this.http.post<T>(`${this.url}${this.getActionValue(action)}`, item);
+  }
+
+  put<T>(id: string, itemToUpdate: Object, action: string = ""): Observable<T> {
+    return this.http.put<T>(`${this.url}${this.getActionValue(action)}/${id}`, itemToUpdate);
+  }
+
+  delete<T>(id: string, action: string = ""): Observable<T> {
+    return this.http.delete<T>(`${this.url}${this.getActionValue(action)}/${id}`);
+  }
+
+  private getActionValue(action: string): string {
+    return `${action ? "/" + action : ""}`;
+  }
+
+  getFormData(object: Object): FormData {
+    const formData = new FormData;
+    Object.keys(object).forEach(key => {
+      const data = object[key as keyof object];
+      if (Array.isArray(data)) {
+        const newData = Array.from(data);
+        newData.forEach(value =>
+          formData.append(key + [], value as keyof object)
+        );
+      } else {
+        formData.append(key, object[key as keyof object]);
+      };
+    });
+    return formData;
+  }
+}
